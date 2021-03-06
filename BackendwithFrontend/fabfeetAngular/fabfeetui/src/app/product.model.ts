5e6e8726718f4186f06293c06cb5d9e2f098c36c
aharnalaha98 <77390401+aharnalaha98@users.noreply.github.com>
2021-03-06 20:45:36 +0530

    Delete BackendwithFrontend directory

--- BackendwithFrontend/fabfeetAngular/fabfeetui/src/app/product.model.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-    constructor(public pid?:string,
-        public pname?:string,
-        public price?:number,
-        public stock?:number,
-        public status?:string,
-        public image?:string){}
-}
